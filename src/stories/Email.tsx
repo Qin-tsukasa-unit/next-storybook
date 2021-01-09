d2860abb81696513bd57dc7003a9cf69de9ff964
github akifumi <aki.suzu.1126@gmail.com>
2021-01-09 20:44:20 +0900

    #1Emailテキストコンポーネントの追加

--- src/stories/Email.tsx
+++ src/stories/Email.tsx
@@ -1,40 +1,38 @@
-import React,{ useState } from 'react';
-import './email.css';
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react'
+import './email.css'
+import { useForm } from 'react-hook-form'
 
 export interface EmailProps {
-
-  title? : string;
+  mail_text?: string
 }
 
-export const Email: React.FC<EmailProps> = () => {
+export const Email: React.FC<EmailProps> = ({ mail_text }) => {
   const { register, handleSubmit, errors, reset } = useForm<EmailProps>()
 
   const handleOnSubmit = (data: EmailProps) => {
-      console.log(data.title);
-      reset()
+    console.log(data.mail_text)
+    reset()
   }
 
   return (
     <div>
-            <form onSubmit={handleSubmit(handleOnSubmit)}>
-                <label>Email </label>
-                <input
-                    type="text"
-                    className={errors.title && 'error'}
-                    name="title"
-                    ref={register({
-                        required: 'メールアドレスを入力してください。',
-                        pattern: {
-                          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                          message: "emailの形式で入力してください",
-                        }
-                    })}
-                />
-                { errors.title && <span className="error-message">{ errors.title.message }</span> }
-                <button className="btn is-primary">追加</button>
-            </form>
-        </div>
-    
-  );
-};
+      <form onBlur={handleSubmit(handleOnSubmit)}>
+        <label>Email </label>
+        <input
+          type="text"
+          className={errors.mail_text && 'error'}
+          name="mail_text"
+          ref={register({
+            required: 'メールアドレスを入力してください。',
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+              message: 'emailの形式で入力してください'
+            }
+          })}
+          value={mail_text}
+        />
+        {errors.mail_text && <span className="error-message">{errors.mail_text.message}</span>}
+      </form>
+    </div>
+  )
+}
